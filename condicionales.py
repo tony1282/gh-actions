6b8419d57c0891fbcd5284d6917162ae6c68c7b4
antony <antonydelacruzramos@gmail.com>
2026-06-23 21:55:10 -0600

    prueba 12

--- condicionales.py
+++ condicionales.py
@@ -1,11 +1,11 @@
 import os
 
-nombre = os.getenv("NAME")
-edad = int(os.getenv("EDAD"))
+name = os.getenv("NOMBRE")
+age = int(os.getenv("EDAD"))
 
 
 
-if edad >= 18:
+if age >= 18:
     print("Eres mayor de edad")
 else:
     print("Eres menor de edad")
